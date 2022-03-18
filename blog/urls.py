e2f6f6b764f2cdc2d31f26fb771714b91c11ccf1
semi-cloud <sksmsrkdtpal@naver.com>
2022-03-18 17:11:44 +0900

    refactor: blog를 CBV로 리팩토링

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views     # 상대 경로 사용 권장
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:pk>/', views.single_post_page),  # blog/1
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    # path('', views.index),
+    # path('<int:pk>/', views.single_post_page),  # blog/1
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 # Create your views here.
-# (실행 경로)blog dir => templates dir => blog dir => index.html (django convention)
+# (실행 경로)blog dir => templates dir => blog dir => post_list.html (django convention)
 # dictionary key 값 - html 파일의 변수 연동
 
-def index(request):
-    # db post 가져오기
-    posts = Post.objects.all().order_by('-pk')
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts
-        }
-    )
+# def index(request):
+#     # db post 가져오기
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts': posts
+#         }
+#     )
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post
+#         }
+#     )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post
-        }
-    )
+class PostList(ListView):
+    model = Post
+
+class PostDetail(DetailView):
+    model = Post
 
