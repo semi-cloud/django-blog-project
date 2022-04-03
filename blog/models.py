196b9cfa114f23112b8187eacd35b82618faafd7
semi-cloud <sksmsrkdtpal@naver.com>
2022-04-03 17:28:23 +0900

    feat: hook 메시지 추가

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_msg = models.TextField(blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)  # NULL 허용
