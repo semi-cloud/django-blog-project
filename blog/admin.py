5413116de23d17d9821725f71cea446928f22b50
semi-cloud <sksmsrkdtpal@naver.com>
2022-04-08 17:23:40 +0900

    feat: tag 모델 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 admin.site.register(Post)
 
 class CategoryAdmin(admin.ModelAdmin):
     #기본 값 설정
     prepopulated_fields = {'slug': ('name',)}
-# Register your models here.
-
 admin.site.register(Category, CategoryAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    #기본 값 설정
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Tag, TagAdmin)
