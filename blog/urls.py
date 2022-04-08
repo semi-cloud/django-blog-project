75cbd8f952419ad4dfbfaa52826512b205aa2b41
semi-cloud <sksmsrkdtpal@naver.com>
2022-04-08 17:13:58 +0900

    feat: 카테고리로 가는 링크와 보기 페이지 추가

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views     # 상대 경로 사용 권장
 urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.show_category_posts)
     # path('', views.index),
     # path('<int:pk>/', views.single_post_page),  # blog/1
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from .models import Post
+from .models import Post, Category
 # Create your views here.
 # (실행 경로)blog dir => templates dir => blog dir => post_list.html (django convention)
 # dictionary key 값 - html 파일의 변수 연동
@@ -29,7 +29,42 @@ from .models import Post
 
 class PostList(ListView):
     model = Post
+    ordering = '-pk'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+
+        return context
 
 class PostDetail(DetailView):
     model = Post
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+
+        return context
+
+
+def show_category_posts(request, slug):
+    if slug == 'no-category':   # 미분류 글
+        category = "미분류"
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug = slug)
+        post_list = Post.objects.filter(category = category)
+    context = {
+        'categories': Category.objects.all(),
+        'no_category_post_count': Post.objects.filter(category=None).count(),
+        'category': category,
+        'post_list': post_list
+    }
+
+    return render(
+        request,
+        'blog/post_list.html',
+        context
+    )
