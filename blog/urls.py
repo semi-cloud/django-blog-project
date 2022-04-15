4012778fcd23a3385c897ae6683ed079587f4e66
semi-cloud <sksmsrkdtpal@naver.com>
2022-04-15 17:43:00 +0900

    feat: 글 수정 기능 및 뷰 추가

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
     path('category/<str:slug>/', views.show_category_posts),
     path('tag/<str:slug>/', views.tag_page),
-    path('create_post/', views.PostCreate.as_view())
+    path('create_post/', views.PostCreate.as_view()),
+    path('update_post/<int:pk>', views.PostUpdate.as_view())
     # path('', views.index),
     # path('<int:pk>/', views.single_post_page),  # blog/1
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .models import Post, Category, Tag
 # Create your views here.
@@ -34,9 +35,11 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     fields = ['title', 'hook_msg', 'content', 'head_image', 'attached_file', 'category']
 
     # 뷰 접근 전 체크
+    # Get
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
 
+    # Post
     def form_valid(self, form):
         current_user = self.request.user
         if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
@@ -44,6 +47,21 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
             return super(PostCreate, self).form_valid(form)
         else:
             return redirect('/blog')
+
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'hook_msg', 'content', 'head_image', 'attached_file', 'category']
+
+    template_name = "blog/post_form_update.html"
+
+    # Get + Post
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
 
 class PostList(ListView):
     model = Post
@@ -104,3 +122,4 @@ def tag_page(request, slug):
         'blog/post_list.html',
         context
     )
+
